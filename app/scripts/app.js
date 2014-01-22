b3adb9dde4395f23fac87d940fa4b9f994b6e4ab
Serkan Sökmen <serkan.sokmen@metglobal.com>
2014-01-23 01:07:42 +0200

    config updates and cleanup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,36 +1,44 @@
 'use strict';
 
 angular.module('fingerprintsApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngAnimate',
-  'colorpicker.module',
-  'ui.router',
-  'firebase'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngAnimate',
+    'colorpicker.module',
+    'ui.router',
+    'firebase'
 ]).
 config(function ($stateProvider, $urlRouterProvider){
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'views/home.html',
-      controller: 'HomeCtrl'
-    })
-    .state('stamps', {
-      url: '/stamps',
-      templateUrl: 'views/stamps/base.html'
-    })
-    .state('stamps.list', {
-      url: '/list',
-      templateUrl: 'views/stamps/list.html',
-      controller: 'StampsController'
-    })
-    .state('stamps.form', {
-      url: '/form',
-      templateUrl: 'views/stamps/form.html',
-      controller: 'StampFormController'
-    });
+    $stateProvider.
+        state('home', {
+            url: '/',
+            templateUrl: 'views/home.html',
+            controller: 'HomeCtrl'
+        }).
+        state('stamps', {
+            url: '/stamps',
+            templateUrl: 'views/stamps/base.html'
+        }).
+        state('stamps.list', {
+            url: '/list',
+            templateUrl: 'views/stamps/list.html',
+            controller: 'StampsController'
+        }).
+        state('stamps.form', {
+            url: '/form',
+            templateUrl: 'views/stamps/form.html',
+            controller: 'StampFormController'
+        });
 
     $urlRouterProvider.otherwise('/');
 }).
-constant('FB_URL', "https://fingerprints.firebaseio.com");
+constant('FB_URL', 'https://fingerprints.firebaseio.com').
+run(function ($rootScope, PresenceService){
+    $rootScope.totalViewers = 0;
+    $rootScope.$on('onOnlineUser', function() {
+        $rootScope.$apply(function() {
+            $rootScope.totalViewers = PresenceService.getOnlineUserCount();
+        });
+    });
+});
